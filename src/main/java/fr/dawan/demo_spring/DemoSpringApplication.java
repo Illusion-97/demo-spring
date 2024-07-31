7a9514f70943adf78d75ef34b7f0a668651a5b13
Yanis ADEKALOM <yadekalom@gmail.com>
2024-07-31 17:13:16 +0200

    first commit

---
+++ src/main/java/fr/dawan/demo_spring/DemoSpringApplication.java
@@ -0,0 +1,23 @@
+package fr.dawan.demo_spring;
+
+import fr.dawan.models.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@SpringBootApplication
+public class DemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+
+	@EventListener(ApplicationStartedEvent.class)
+	public void init() {
+		User user = new User().age(17).nom("ADEKALOM").prenom("Yanis");
+		log.debug("user = \u001B[31m" + user + "\u001B[0m");
+	}
+}
